68527d3d42b0e4867170a37e4020699ba93b9a6d
Rezzylol <101850992+Rezzylol@users.noreply.github.com>
2023-11-05 14:50:43 +1300

    Update GMBOT.py
    removed the debugging stuff

--- GMBOT.py
+++ GMBOT.py
@@ -6,37 +6,31 @@ bot = telebot.TeleBot(API_KEY, parse_mode=None)
 
 @bot.message_handler(commands=["start"])
 def send_help_message(msg):
-    bot.reply_to(msg, "Test")
+    bot.reply_to(msg, "omg haiiiii :3c")
 
 
 @bot.message_handler(commands=["goodmorning"])
 def send_help_message(msg):
-    print(msg)
     bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
 
 @bot.message_handler(func=lambda msg: msg.text == "good morning")
 def send_help_message(msg):
-    print(msg)
     bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
 
 @bot.message_handler(func=lambda msg: msg.text == "gm")
 def send_help_message(msg):
-    print(msg)
     bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
 
 @bot.message_handler(func=lambda msg: msg.text == "Good Morning")
 def send_help_message(msg):
-    print(msg)
     bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
 
 @bot.message_handler(func=lambda msg: msg.text == "Good morning")
 def send_help_message(msg):
-    print(msg)
     bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
 
 @bot.message_handler(func=lambda msg: msg.text == "GM")
 def send_help_message(msg):
-    print(msg)
     bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
 
 
