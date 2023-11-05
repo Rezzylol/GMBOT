5b7d80bb761394245eaa18fc0649c7d6bec45ebc
Rezzylol <101850992+Rezzylol@users.noreply.github.com>
2023-11-05 16:09:19 +1300

    Update GMBOT.py
    Changed help messages to multi messages so they work in group chats

--- GMBOT.py
+++ GMBOT.py
@@ -14,23 +14,23 @@ def send_help_message(msg):
     bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
 
 @bot.message_handler(func=lambda msg: msg.text == "good morning")
-def send_help_message(msg):
+def send_multi_message(msg):
     bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
 
 @bot.message_handler(func=lambda msg: msg.text == "gm")
-def send_help_message(msg):
+def send_multi_message(msg):
     bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
 
 @bot.message_handler(func=lambda msg: msg.text == "Good Morning")
-def send_help_message(msg):
+def send_multi_message(msg):
     bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
 
 @bot.message_handler(func=lambda msg: msg.text == "Good morning")
-def send_help_message(msg):
+def send_multi_message(msg):
     bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
 
 @bot.message_handler(func=lambda msg: msg.text == "GM")
-def send_help_message(msg):
+def send_multi_message(msg):
     bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
 
 
