9229a407f173b2dcb691d8fbacb5d73fcef4d346
Rezzylol <101850992+Rezzylol@users.noreply.github.com>
2023-11-05 14:27:47 +1300

    Create GMBOT.py

---
+++ GMBOT.py
@@ -0,0 +1,22 @@
+import telebot
+from telebot import types
+from constants import API_KEY
+
+bot = telebot.TeleBot(API_KEY, parse_mode=None)
+
+@bot.message_handler(commands=["start"])
+def send_help_message(msg):
+    bot.reply_to(msg, "Test")
+
+
+@bot.message_handler(commands=["goodmorning"])
+def send_help_message(msg):
+    print(msg)
+    bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
+
+
+bot.polling()
+
+
+#daily check in
+
