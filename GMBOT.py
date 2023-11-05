2644fa79766b0cb778c4bc232209ee2d6828ccec
Rezzylol <101850992+Rezzylol@users.noreply.github.com>
2023-11-05 14:31:50 +1300

    Update GMBOT.py
    made a very lazy way to have a bunch of different ways of saying good morning

--- GMBOT.py
+++ GMBOT.py
@@ -14,9 +14,27 @@ def send_help_message(msg):
     print(msg)
     bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
 
+@bot.message_handler(func=lambda msg: msg.text == "good morning")
+def send_help_message(msg):
+    print(msg)
+    bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
+
+@bot.message_handler(func=lambda msg: msg.text == "gm")
+def send_help_message(msg):
+    print(msg)
+    bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
+
+@bot.message_handler(func=lambda msg: msg.text == "Good Morning")
+def send_help_message(msg):
+    print(msg)
+    bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
+
+@bot.message_handler(func=lambda msg: msg.text == "Good morning")
+def send_help_message(msg):
+    print(msg)
+    bot.reply_to(msg, f"Good Morning! {msg.from_user.first_name} youve checked in 0/0")
 
 bot.polling()
 
 
 #daily check in
-
