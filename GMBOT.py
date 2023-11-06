8ca7dca53e421a42dd36db8ab4a7f6c50cd082fb
Keikuris <125270064+keikuris@users.noreply.github.com>
2023-11-06 18:10:16 +1300

    refactor, working
    streaks untested

--- GMBOT.py
+++ GMBOT.py
@@ -1,71 +1,98 @@
 import telebot
-import datetime
 import os
 import csv
+from datetime import datetime
+from collections import defaultdict
 
-print(f"Bot started at: {datetime.datetime.now()}")
+print(f"{datetime.now()} init")
 
 API_KEY = os.getenv('API_KEY')
 bot = telebot.TeleBot(API_KEY)
 
-user_data = {}
-checkin_data_path = '/data/checkin_data.csv'
-if os.path.exists(checkin_data_path):
-    with open(checkin_data_path, 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            user_id = int(row['user_id'])
-            date = row['date']
-            checkin_count = int(row['checkin_count'])
-            if user_id not in user_data:
-                user_data[user_id] = {'username': row['username'], 'streak': 0, 'total_checkins': 0, 'last_checkin_date': None, 'attempt_count': 0}
-            user_data[user_id]['total_checkins'] += checkin_count
-            if user_data[user_id]['last_checkin_date'] == date:
-                user_data[user_id]['streak'] += 1
-            else:
-                user_data[user_id]['streak'] = 1
-            user_data[user_id]['last_checkin_date'] = date
-            user_data[user_id]['attempt_count'] = 0
+check_ins_file = '/data/check_ins.csv'
+
+if not os.path.isfile(check_ins_file):
+    with open(check_ins_file, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['username', 'date'])
 
 @bot.message_handler(commands=['start'])
-def handle_start(message):
-    print(f"{datetime.datetime.now()} {message.from_user.username} /start")
+def send_welcome(message):
+    print(f"{datetime.now()} {message.from_user.username} {message.text}")
+    bot.reply_to(message, "omg haiiiii :3c")
 
-    bot.send_message(message.chat.id, "omg haiiiii :3c")
-
-@bot.message_handler(func=lambda message: message.text.lower() in ['good morning', 'gm', 'good morning beverage', 'gm beverage'])
-def handle_checkin(message):
-    print(f"{datetime.datetime.now()} {message.from_user.username} gm")
-    user_id = message.from_user.id
+@bot.message_handler(func=lambda message: message.text.lower() in ['good morning', 'gm', 'good morning beverage', 'gm beverage', 'good rawrning', 'good morning team', 'good morningverage'])
+def check_in(message):
+    print(f"{datetime.now()} {message.from_user.username} {message.text}")
     username = message.from_user.username
-    today = datetime.date.today().strftime('%Y-%m-%d')
-        else:
-            user_data[user_id]['streak'] = 1
-            user_data[user_id]['attempt_count'] = 1
-        user_data[user_id]['last_checkin_date'] = today
-
-        with open(checkin_data_path, 'a', newline='') as csvfile:
-            fieldnames = ['user_id', 'username', 'date', 'checkin_count']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerow({'user_id': user_id, 'username': username, 'date': today, 'checkin_count': 1})
+    now = datetime.now().date()
+    check_ins_today = 0
+    with open(check_ins_file, 'r+', newline='') as file:
+        reader = csv.reader(file)
+        writer = csv.writer(file)
+        check_ins = [row for row in reader]
+        for row in check_ins:
+            if row[0] == username and row[1] == str(now):
+                check_ins_today += 1
+        writer.writerow([username, now])
+    today_check_ins = check_ins_today + 1
+    if today_check_ins == 1:
+        streak = 0
+        total_check_ins = 0
+        last_check_in_date = None
+        with open(check_ins_file, 'r') as file:
+            reader = csv.reader(file)
+            sorted_check_ins = sorted([row for row in reader if row[0] == username], key=lambda row: row[1])
+            for row in sorted_check_ins:
+                check_in_date = datetime.strptime(row[1], '%Y-%m-%d').date()
+                if last_check_in_date is None or (check_in_date - last_check_in_date).days == 1:
+                    streak += 1
+                elif (check_in_date - last_check_in_date).days > 1:
+                    streak = 1
+                last_check_in_date = check_in_date
+                total_check_ins += 1
+        reply = f"Good morning, @{username}! You've been checked in for today. {streak} days in a row so far."
+    elif today_check_ins == 69:
+        reply = "nice." # kei's easter egg
+    else:
+        reply = f"Good morning again, @{username}! Love the enthusiasm, you've tried to check in, like, {today_check_ins} times today now."
+    bot.reply_to(message, reply)
 
 @bot.message_handler(commands=['leaderboard'])
-def handle_leaderboard(message):
-    print(f"{datetime.datetime.now()} {message.from_user.username} /leaderboard")
-    sorted_streaks = sorted(user_data.items(), key=lambda x: x[1]['streak'], reverse=True)[:5]
-    leaderboard_message = "Streak:\n"
-    for rank, (user_id, data) in enumerate(sorted_streaks, start=1):
-        username = data['username']
-        streak = data['streak']
-        leaderboard_message += f"{rank}. @{username} - {streak} days\n"
-    leaderboard_message += "\n"
-    sorted_checkins = sorted(user_data.items(), key=lambda x: x[1]['total_checkins'], reverse=True)[:5]
-    leaderboard_message += "Check-ins:\n"
-    for rank, (user_id, data) in enumerate(sorted_checkins, start=1):
-        username = data['username']
-        total_checkins = data['total_checkins']
-        leaderboard_message += f"{rank}. @{username} - {total_checkins}\n"
-    bot.send_message(message.chat.id, leaderboard_message)
+def leaderboard(message):
+    print(f"{datetime.now()} {message.from_user.username} {message.text}")
+    user_data = defaultdict(lambda: {'streak': 0, 'total': 0, 'last_check_in': None, 'days_checked_in': set()})
+    with open(check_ins_file, 'r') as file:
+        reader = csv.reader(file)
+        next(reader, None)
+        for username, date_str in reader:
+            date = datetime.strptime(date_str, '%Y-%m-%d').date()
+            user = user_data[username]
+            if user['last_check_in'] is None or (date - user['last_check_in']).days == 1:
+                user['streak'] += 1
+            elif (date - user['last_check_in']).days > 1:
+                user['streak'] = 1
+            user['last_check_in'] = date
+            if date_str not in user['days_checked_in']:
+                user['total'] += 1
+                user['days_checked_in'].add(date_str)
+    top_streaks = sorted(user_data.items(), key=lambda item: item[1]['streak'], reverse=True)[:5]
+    top_totals = sorted(user_data.items(), key=lambda item: item[1]['total'], reverse=True)[:5]
+    streaks_message = "\n".join([f"{idx + 1}. @{username} - {data['streak']}" for idx, (username, data) in enumerate(top_streaks)])
+    totals_message = "\n".join([f"{idx + 1}. @{username} - {data['total']}" for idx, (username, data) in enumerate(top_totals)])
+    reply = f"🏆 Streaks:\n{streaks_message}\n\n🏆 Check-ins:\n{totals_message}"
+    bot.reply_to(message, reply)
 
-if __name__ == "__main__":
-    bot.polling(none_stop=True)
+@bot.message_handler(func=lambda message: message.from_user.username == "majeflyer")
+def rezzy(message):
+    if (random.randint(1,2500)) == 55:
+        print(f"{datetime.now()} rezzy's easter egg #1")
+        bot.reply_to(message, "alright alright thats enough")
+
+@bot.message_handler(func=lambda m: True)
+def sabre(message):
+    if (random.randint(1,1000)) == 55:
+        print(f"{datetime.now()} rezzy's easter egg #2")
+        bot.reply_to(message, "thoughts? @SabreDance")
+
+bot.polling()
