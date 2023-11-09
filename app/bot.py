dcf48847cc3a5e616beb7fd0fcc629f001773e78
Keikuris <keikuris.fur@gmail.com>
2023-11-10 11:49:33 +1300

    adjust init message

--- app/bot.py
+++ app/bot.py
@@ -41,10 +41,10 @@ def log_to_control_chat(message):
 
 response = requests.get(URL_REPO)
 if response.ok:
-    commit_date = response.json()['commit']['committer']['date']
+    commit_date = response.json()['commit']['committer']['date'].strftime("%Y-%m-%d %H:%M:%S")
     commit_message = response.json()['commit']['message']
     commit_sha = response.json()['sha'][:7]
-    log_to_control_chat(f"```init {VERSION}\n{commit_sha} - {commit_date}\n{commit_message}```")
+    log_to_control_chat(f"```{VERSION}\n{commit_sha} - {commit_date}\n{commit_message}```")
 
 for file_path in [FILE_ATTEMPTS, FILE_CHECK_INS, FILE_CREDITS, FILE_IGNORE_LIST]:
     if not os.path.isfile(file_path):
